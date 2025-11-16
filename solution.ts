69c0ac4329f4b2bfb632894c902a7a27249948f0
ranajitchandra <ranajitchandradas3@gmail.com>
2025-11-16 21:56:23 +0600

    problem solve-7

--- solution.ts
+++ solution.ts
@@ -104,3 +104,12 @@ const myBook: TBook = {
   isAvailable: true,
 };
 
+// Problem-6
+
+function getUniqueValues (arr1: number[], arr2: number[]){
+    return Array.from(new Set([...arr1, ...arr2]))
+}
+
+const array1 = [1, 2, 3, 4, 5,5,5,5,5,5,3,3,3,];
+const array2 = [3, 4, 5, 6, 7,8,8,8,9,10,10,12,19];
+console.log(getUniqueValues(array1, array2));
